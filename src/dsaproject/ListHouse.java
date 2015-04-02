e7bebe714e4b0b2ec809534887c0195a04a62342
Imeyshan <imeyshan@live.com>
2015-04-02 13:34:39 +0530

    list house Property  Added By Nuwan

--- src/dsaproject/ListHouse.java
+++ src/dsaproject/ListHouse.java
@@ -10,6 +10,132 @@ package dsaproject;
  *
  * @author Imeyshan
  */
-public class ListHouse {
+public class ListHouse implements IListTable{
+
+    private int LotNumber = 0;
+    private String FirstName = null;
+    private String LastName = null;
+    private double Price = 0;
+    private int SquareFeet = 0;
+    private int NoOFBedRooms = 0;
+
+    /**
+     * @return the LotNumber
+     */
+    public int getLotNumber() {
+        return LotNumber;
+    }
+
+    /**
+     * @param LotNumber the LotNumber to set
+     */
+    public void setLotNumber(int LotNumber) {
+        this.LotNumber = LotNumber;
+    }
+
+    /**
+     * @return the FirstName
+     */
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    /**
+     * @param FirstName the FirstName to set
+     */
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    /**
+     * @return the LastName
+     */
+    public String getLastName() {
+        return LastName;
+    }
+
+    /**
+     * @param LastName the LastName to set
+     */
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    /**
+     * @return the Price
+     */
+    public double getPrice() {
+        return Price;
+    }
+
+    /**
+     * @param Price the Price to set
+     */
+    public void setPrice(double Price) {
+        this.Price = Price;
+    }
+
+    /**
+     * @return the SquareFeet
+     */
+    public int getSquareFeet() {
+        return SquareFeet;
+    }
+
+    /**
+     * @param SquareFeet the SquareFeet to set
+     */
+    public void setSquareFeet(int SquareFeet) {
+        this.SquareFeet = SquareFeet;
+    }
+
+    /**
+     * @return the NoOFBedRooms
+     */
+    public int getNoOFBedRooms() {
+        return NoOFBedRooms;
+    }
+
+    /**
+     * @param NoOFBedRooms the NoOFBedRooms to set
+     */
+    public void setNoOFBedRooms(int NoOFBedRooms) {
+        this.NoOFBedRooms = NoOFBedRooms;
+    }
+
+    @Override
+    public ListHouse Copy() {
+            
+       ListHouse list=new ListHouse();
+       return list;
+        
+        
+    }
+
+
     
+    @Override
+    public int compareTo(ListHouse list) {
+       
+        if (list.getLotNumber()>this.getLotNumber()) {
+            
+            return -1;
+        }
+        else if (list.getLotNumber()==this.getLotNumber()) {
+            
+            return 0;
+        }
+        else
+        {
+            return  1;
+        }
+                  
+        
+ 
+       
+    }
+    
+ 
+
 }
+
